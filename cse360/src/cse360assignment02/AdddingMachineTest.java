c736ddf340be1cb6945668e2ad42005a695e20e6
rward <rward@MESTABURRITO>
2020-10-07 20:14:10 -0700

    Newest commit [finished AddingMachine]

---
+++ cse360/src/cse360assignment02/AdddingMachineTest.java
@@ -0,0 +1,15 @@
+package cse360assignment02;
+
+public class AdddingMachineTest {
+	public static void main(String[] args) {
+		AddingMachine myCalculator = new AddingMachine();
+		myCalculator.add(4);
+		myCalculator.subtract(2);
+		myCalculator.add(5);
+		System.out.println("Total : " + myCalculator.getTotal());
+		
+		System.out.println(myCalculator.toString());
+		
+	}
+
+}
--- cse360/src/cse360assignment02/AddingMachine.java
+++ cse360/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,32 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String str = "0";
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+	  total = total + value;
+	  str = str + " + " + value;
   }
 
   public void subtract (int value) {
+	  total = total - value;
+	  str = str + " - " + value;
   }
 
   public String toString () {
-    return "";
+    return str;
   }
 
   public void clear() {
+	  total = 0;
+	  
   }
 }
